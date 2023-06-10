096c7728f7dcd925225698af1ce141b87c50c5e9
Yunika-Bajracharya <076bct095.yunika@pcampus.edu.np>
2023-06-10 18:54:01 +0545

    Set up react app + sidebar

---
+++ niti-client/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <aside className="sidebar">
+        <h1 className="logo">Niti</h1>
+        <div className="sidebar-button">New Chat</div>
+      </aside>
+
+      <section className="chatbox"></section>
+    </div>
+  );
+}
+
+export default App;
