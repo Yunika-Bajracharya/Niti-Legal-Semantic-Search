f3baf94c3062298dd551d508e706baa8fab2b00f
Yunika-Bajracharya <076bct095.yunika@pcampus.edu.np>
2023-07-02 00:26:35 +0545

    Style message interface

--- niti-client/src/App.js
+++ niti-client/src/App.js
@@ -14,10 +14,32 @@ function App() {
       </aside>
 
       <section className="chatbox">
+        <div className="chat-log">
+          <div className="chat-query">
+            <div className="avatar">
+              <img src={require("./assets/user.png")} alt="logo" />
+            </div>
+            <div className="message">
+              Is it legal to marry one's favorite fictional character?
+            </div>
+          </div>
+
+          <div className="chat-answer">
+            <div className="avatar">
+              <img src={require("./assets/logo.png")} alt="logo" />
+            </div>
+            <div className="message">
+              Unfortunately, the legal system isn't ready for inter-dimentional
+              love affairs just yet.
+            </div>
+          </div>
+        </div>
+
         <div className="chat-inputbox">
           <textarea
             rows="1"
-            className="chat-input-textarea"
+            className="input-textarea"
+            type="text"
             placeholder="Type your queries here"
           ></textarea>
         </div>
