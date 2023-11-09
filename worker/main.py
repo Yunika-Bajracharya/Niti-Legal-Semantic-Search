e07800e5ffbd75695dba2add638e63f1a659cc41
Pranab  Gubhaju <59060737+Daskhas@users.noreply.github.com>
2023-11-09 16:17:39 +0545

    Fixed multiple message issue

--- worker/main.py
+++ worker/main.py
@@ -24,39 +24,53 @@ async def main():
         response = await consumer.consume_stream(stream_channel="message_channel", count=1, block=0)
 
         if response:
+            print("Response",response)
             for stream, messages in response:
                 # Get message from stream, and extract token, message data and message id
                 for message in messages:
                     message_id = message[0]
                     token = [k.decode('utf-8') for k, v in message[1].items()][0]
                     message = [v.decode('utf-8') for k, v in message[1].items()][0]
-
+                    
                     # Create a new message instance and add to cache, specifying the source as human
-                    msg = Message(msg=message)                 
-                 
-                    await cache.add_message_to_cache(token=token, source="human", message_data=msg.dict())
+                    try:
 
-                    # Get chat history from cache
-                    data = await cache.get_chat_history(token=token)
 
-                    # Clean message input and send to query
-                    message_data = data['messages'][-3:]
+                        msg = Message(msg=message) 
 
-                    input = ["" + i['msg'] for i in message_data]
-                    input = " ".join(input)
+                        await cache.add_message_to_cache(token=token, source="human", message_data=msg.dict())
+                        
+                        res = Model().query(message = message) 
+                            # # Get chat history from cache
+                        # data = await cache.get_chat_history(token=token)
+                        # print("Data:",data)
+                        # # Clean message input and send to query
+                        # message_data = data['messages'][-4:]
+
+                        # print(message_data)
+
+                        # input = ["" + i['msg'] for i in message_data]
+                        # input = " ".join(input)
+                        # print(input)
+
+                        
+                        # res = "Ooolaaa"        
+
+                        msg = Message(
+                            msg=res
+                        )
+
+                        stream_data = {}
+                        stream_data[str(token)] = str(msg.dict())
+
+                        await producer.add_to_stream(stream_data, "response_channel")
+                        
+                        await cache.add_message_to_cache(token=token, source="bot", message_data=msg.dict())   
+
+                    except Exception as e:
+                        print("Exception",e)
+                        print("Cant find valid token : Token has expired")
                     
-                    res = Model().query(message = input)
-
-                    msg = Message(
-                        msg=res
-                    )
-
-                    stream_data = {}
-                    stream_data[str(token)] = str(msg.dict())
-
-                    await producer.add_to_stream(stream_data, "response_channel")
-                    
-                    await cache.add_message_to_cache(token=token, source="bot", message_data=msg.dict())
 
                 # Delete messaage from queue after it has been processed
                 await consumer.delete_message(stream_channel="message_channel", message_id=message_id)
@@ -64,3 +78,5 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+
+
